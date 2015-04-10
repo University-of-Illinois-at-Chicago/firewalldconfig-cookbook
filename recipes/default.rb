a085ed9bda71245ecc7b4c05ad1c783f5517fae3
Johnathan Kupferer <jtk@uic.edu>
2015-04-10 00:24:27 -0400

    Update default recipe to install nokogiri.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,11 @@
 #
 # Copyright:: 2015, The University of Illinois at Chicago
 
-package 'firewalldconfig'
+package 'firewalld'
+chef_gem 'nokogiri' do
+  compile_time false
+end
 
 service "firewalld" do
   action [:enable, :start]
-end
-
-firewalldconfig "/etc/firewalld/firewalld.conf" do
-  notifies :reload, "service[firewalld]"
 end
