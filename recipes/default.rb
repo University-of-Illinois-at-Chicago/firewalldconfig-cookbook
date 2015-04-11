f00c7540f91fb2988a694c8d28502f0129f90a43
Johnathan Kupferer <jtk@uic.edu>
2015-04-11 19:01:11 -0400

    Add excecute firewalld-reload.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,12 @@ package 'firewalld'
 service "firewalld" do
   action [:enable, :start]
 end
+
+# Use this for reload. At the moment, the firewalld service reload can
+# crash firewalld! This is because the the firewalld reload sends a HUP
+# signal and, at present, if it receives a second HUP before it finishes
+# processing the first then it dies!
+execute "firewalld-reload" do
+  command "firewall-cmd --reload"
+  action :nothing
+end
