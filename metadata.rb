b32f018369f0fbfb0e72526815881ea29442267b
Johnathan Kupferer <jtk@uic.edu>
2015-04-13 15:19:48 -0500

    Set release version 0.1.8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jtk@uic.edu'
 license          'Apache v2.0'
 description      'Installs/Configures firewalld using attributes and config files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '0.1.8'
 
 depends          'xml'
 
