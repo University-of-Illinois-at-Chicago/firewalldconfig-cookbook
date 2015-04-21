fc5a089a7f4ccad4bd62d0f675cc5494c823b623
Johnathan Kupferer <jtk@uic.edu>
2015-04-21 09:28:14 -0500

    Move record action to ruby_block.

--- recipes/record.rb
+++ recipes/record.rb
@@ -4,20 +4,22 @@
 #
 # Copyright:: 2015, The University of Illinois at Chicago
 
-Chef::Provider::Firewalldconfig.read_conf.each do |k, v|
-  node.set['firewalld'][k] = v
+ruby_block 'firewalldconfig-record' do
+  action :run
+  block do
+    Chef::Provider::Firewalldconfig.read_conf.each do |k, v|
+      node.set['firewalld'][k] = v
+    end
+    node.set['firewalld']['services'] = {}
+    Chef::Provider::FirewalldconfigService.configured.each do |name|
+      node.set['firewalld']['services'][name] =
+        Chef::Provider::FirewalldconfigService.read_configuration(name)
+    end
+    node.set['firewalld']['zones'] = {}
+    Chef::Provider::FirewalldconfigZone.configured.each do |name|
+      node.set['firewalld']['zones'][name] =
+        Chef::Provider::FirewalldconfigZone.read_configuration(name)
+    end
+    node.save
+  end
 end
-
-node.set['firewalld']['services'] = {}
-Chef::Provider::FirewalldconfigService.configured.each do |name|
-  node.set['firewalld']['services'][name] =
-    Chef::Provider::FirewalldconfigService.read_configuration(name)
-end
-
-node.set['firewalld']['zones'] = {}
-Chef::Provider::FirewalldconfigZone.configured.each do |name|
-  node.set['firewalld']['zones'][name] =
-    Chef::Provider::FirewalldconfigZone.read_configuration(name)
-end
-
-node.save
