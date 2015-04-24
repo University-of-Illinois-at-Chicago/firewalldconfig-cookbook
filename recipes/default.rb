048e8c582567d602d17ad00a08cd592f09e1ffbc
Johnathan Kupferer <jtk@uic.edu>
2015-04-23 23:43:06 -0500

    Improve default recipe to handle more os variants

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,9 +18,37 @@ end
 
 package 'firewalld'
 
+# Make sure that iptables is stopped and disabled.
+service 'iptables' do
+  action [:disable, :stop]
+end
+
+# Mask iptables service so that it cannot be accidentally started.
+execute 'mask iptables.service' do
+  command 'systemctl mask iptables.service'
+  not_if do
+    iptables_service = '/etc/systemd/system/iptables.service'
+    ::File.symlink?(iptables_service) &&
+      ::File.readlink(iptables_service) == '/dev/null'
+  end
+end
+
+# If firewalld is masked, we need to unmask it for actions enable and start
+# to function.
+execute 'unmask firewalld.service' do
+  command 'systemctl unmask firewalld.service'
+  only_if do
+    firewalld_service = '/etc/systemd/system/firewalld.service'
+    ::File.symlink?(firewalld_service) &&
+      ::File.readlink(firewalld_service) == '/dev/null'
+  end
+end
+
 service 'firewalld' do
   action [:enable, :start]
 end
+
+include_recipe 'xml::ruby'
 
 # Use this for reload. At the moment, the firewalld service reload can
 # crash firewalld! This is because the the firewalld reload sends a HUP
