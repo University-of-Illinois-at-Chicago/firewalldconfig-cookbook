985807d909a60370850a98073cda0774c48f90f7
Johnathan Kupferer <jtk@uic.edu>
2016-05-18 10:47:20 -0500

    Fix chef deprecation warnings.
    Move configuration file functions from provider to library.

--- recipes/record.rb
+++ recipes/record.rb
@@ -7,19 +7,22 @@
 ruby_block 'firewalldconfig-record' do
   action :run
   block do
-    Chef::Provider::Firewalldconfig.read_conf.each do |k, v|
+    FirewalldconfigUtil.read_conf.each do |k, v|
       node.set['firewalld'][k] = v
     end
+
     node.set['firewalld']['services'] = {}
-    Chef::Provider::FirewalldconfigService.configured.each do |name|
+    FirewalldconfigUtil.configured_service_names.each do |name|
       node.set['firewalld']['services'][name] =
-        Chef::Provider::FirewalldconfigService.read_configuration(name)
+        FirewalldconfigUtil.read_service_configuration(name)
     end
+
     node.set['firewalld']['zones'] = {}
-    Chef::Provider::FirewalldconfigZone.configured.each do |name|
+    FirewalldconfigUtil.configured_zone_names.each do |name|
       node.set['firewalld']['zones'][name] =
-        Chef::Provider::FirewalldconfigZone.read_configuration(name)
+        FirewalldconfigUtil.read_zone_configuration(name)
     end
+
     node.save
   end
 end
