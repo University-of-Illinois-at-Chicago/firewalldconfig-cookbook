9fece5f0d6cd159d9734d24dda95c45dd96b7bcd
Johnathan Kupferer <jtk@uic.edu>
2016-06-01 11:52:37 -0500

    Bump version to 0.7.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jtk@uic.edu'
 license          'Apache v2.0'
 description      'Installs/Configures firewalld using configuration files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.0'
+version          '0.7.1'
 
 depends          'xml'
 
