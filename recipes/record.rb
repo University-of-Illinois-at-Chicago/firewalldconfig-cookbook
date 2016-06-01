50bd96d3c17fe553812b7074ccfee81debdb2a22
Johnathan Kupferer <jtk@uic.edu>
2016-06-01 11:48:28 -0500

    Fix record recipe when node lacks firewalld attr
    The record recipe was failing because of trying to parse nil as JSON
    text. Check for nil value on firewalld attribute.

--- recipes/record.rb
+++ recipes/record.rb
@@ -5,8 +5,14 @@
 # Copyright:: 2015, The University of Illinois at Chicago
 
 ruby_block 'firewalldconfig-record' do
+
+  # Serialize and standardize attributes from node and system using JSON.
+  node_firewalld_attr = node['firewalld'].nil? ? {} : JSON.parse(node['firewalld'].inspect.gsub(/\=\>/,':'))
+  system_firewalld_attr = JSON.parse(FirewalldconfigUtil.read_all_config().to_json)
+
   # Trigger record of node attributes if out of sync.
-  if JSON.parse(node['firewalld'].inspect.gsub(/\=\>/,':')) == JSON.parse(FirewalldconfigUtil.read_all_config().to_json)
+  if node_firewalld_attr == system_firewalld_attr
+    # No need to update, node attributes already match system configuration.
     action :nothing
   else
     action :run
